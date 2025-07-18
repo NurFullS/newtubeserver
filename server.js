a834197995470a567c571310e7dd9cecd7dfddb3
NurFullS <nurzhenishov10@gmail.com>
2025-07-18 13:03:43 +0600

    server

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const { Pool } = pkg
 
 const pool = new Pool({
   user: process.env.PGUSER,
-  host: process.env.PGLHOST,
+  host: process.env.PGHOST,
   database: process.env.PGDATABASE,
   password: process.env.PGPASSWORD,
   port: process.env.PGPORT
