a209fb93297344a817f51c8eb62823cc2a3bddef
NurFullS <nurzhenishov10@gmail.com>
2025-07-18 12:50:18 +0600

    connect-database

--- server.js
+++ server.js
@@ -1,6 +1,16 @@
 import express from 'express'
 import cors from 'cors'
 
+const { Pool } = require('pg')
+
+const pool = new Pool ({
+  user: process.env.PGUSER,
+  host: process.env.PGLOCALHOST,
+  database: process.env.PGDATABASE,
+  password: process.env.PGPASSWORD,
+  port: process.env.PGPORT
+})
+
 const app = express()
 const PORT = process.env.PORT || 3005
 
